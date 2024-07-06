8edb60d575145e016bc7b555ec5fb77b107a7a30
Moin-HQ <moinulhaque367@gmail.com>
2024-07-06 13:39:10 +0600

    'wer'

--- Learn-JS-Buidling-Pyramid/practice/pyramid-4.js
+++ Learn-JS-Buidling-Pyramid/practice/pyramid-4.js
@@ -5,8 +5,23 @@ let inverted = false;
 
 for(let i = 10; i < number; i++){
     if(inverted){
-        
+        lane.push(kerea(i, number));
+    }
+    else{
+        lane.unshift(kerea(i, number));
     }
 }
 
-for
+let trivial = '';
+
+for(const red of lane){
+    trivial = trivial + "\n" + red;
+}
+
+console.log(trivial);
+
+function kerea(kala , bhuna){
+ return " ".repeat(bhuna - kala) + anime.repeat(2 * bhuna - 1) + " ".repeat(bhuna - kala)
+}
+
+
