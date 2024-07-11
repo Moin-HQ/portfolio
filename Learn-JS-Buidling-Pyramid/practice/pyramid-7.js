b642211c4fb0e488b7e9d4d97cb0f162a9f9fd62
Moin-HQ <moinulhaque367@gmail.com>
2024-07-11 15:50:54 +0600

    erw

---
+++ Learn-JS-Buidling-Pyramid/practice/pyramid-7.js
@@ -0,0 +1,24 @@
+const character = "#";
+const count = 10;
+const rows = [];
+const inverted = true;
+
+for(let i = 1; i < count; i++){
+    if(inverted){
+        rows.push(add(i , count));
+    }
+    else{
+        rows.unshift(add(i , count));
+    }
+}
+
+let result = "";
+for(row of rows){
+    result = result + "\n" + row;
+}
+
+console.log(result);
+
+function add(rowNumber, rowCount){
+    return " ".repeat(rowCount-rowNumber) + character.repeat(2*rowNumber-1) + " ".repeat(rowCount-rowNumber);
+}
