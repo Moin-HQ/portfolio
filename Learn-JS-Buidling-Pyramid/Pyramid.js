0a19aef13455ac88afba65e1f41b5cbd67ce873e
Moin-HQ <moinulhaque367@gmail.com>
2024-08-01 00:20:19 +0600

    er

--- Learn-JS-Buidling-Pyramid/Pyramid.js
+++ Learn-JS-Buidling-Pyramid/Pyramid.js
@@ -10,8 +10,7 @@ function padRow(rowNumber, rowCount) {
 for (let i = 1; i <= count; i++) {
   if (inverted) {
     rows.unshift(padRow(i, count));
-  } els
-  e {
+  } else {
     rows.push(padRow(i, count));
   }
 }
